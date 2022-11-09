4cc924ba08d37c51c1bc04c5801f99cf25a2b787
Tonyg92x <anthony.guay112@gmail.com>
2022-11-09 08:45:43 -0500

    update

--- includes/vector.hpp
+++ includes/vector.hpp
@@ -6,7 +6,7 @@
 /*   By: aguay <aguay@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 07:56:38 by aguay             #+#    #+#             */
-/*   Updated: 2022/11/08 15:06:06 by aguay            ###   ########.fr       */
+/*   Updated: 2022/11/09 08:39:49 by aguay            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,26 @@ namespace ft
     template <typename T, typename alloc = std::allocator<T> >
     class vector
     {
+        typedef unsigned long long  size_type;
+        typedef T                   value_type;
+        typedef std::allocator<T>   allocator_type;
+        
         public :
 
         //  =============== CONTRUCTOR          =============== //
+
+            //  Default constructor -> everything empty
             vector(void) : _nbElement(0), _maxElement(0), _ptr(NULL), _allocator(createAllocator()){}
 
+            //  Fill constructor -> create N element with val as value
+            vector(size_type n, value_type & val) : _nbElement(n), _maxElement(n), _allocator(createAllocator())
+            {
+                fillVector(n, val);
+            }
+
+            //  Range constructor -> create elements from iterrator begin to iterrator end
+
+            //  Copy constructor -> Create the vector as a copy or the vector passed as parameter
         
         //  =============== DESTRUCTOR          =============== //
             ~vector(void)
@@ -32,26 +47,40 @@ namespace ft
             };
 
         //  =============== OPERATOR OVERLOAD   =============== //
+
+            //  Assignation operator overload -> change values to the same value of the vector on the right
+
+            //  Index [ ] operator overload. objectName[x] will get the value of the vector at the position x
         
 
         //  =============== UTILS METHODES      =============== //
-        public :
-            void    addObject(T obj)
+        private :
+
+            //  Only use on fill constructor because it allocate all the memory needed
+            void    fillVector(size_type n, value_type val)
+            {
+                //  Note: allocate x2 might be a problem
+                addMemory();
+                for (size_type x = 0; x <= n; x++)
+                    _ptr[x] = val;
+            }
+        
+            void    addObject(value_type val)
             {
                 if (_nbElement + 1 > _maxElement)
                     addMemory();
                 _nbElement++;
-                std::cout << "Need to add " << obj << " here" << std::endl;
+                std::cout << "Need to add " << val << " here" << std::endl;
             }
 
             void    addMemory(void)
             {
-                T   *newData;
+                value_type   *newData;
 
                 if (_maxElement != 0)
                     newData = _allocator.allocate(_maxElement * 2, 0);
                 else
-                    newData = _allocator.allocate(4, 0);
+                    newData = _allocator.allocate(4, _ptr);
                 //  Copy elements here
                 if (_maxElement > 0)
                     _allocator.deallocate(_ptr, _maxElement);
@@ -59,16 +88,17 @@ namespace ft
                 _ptr = newData;
             }
 
-            std::allocator<T>   createAllocator(void){std::allocator<T>   x;return (x);}
+           allocator_type   createAllocator(void){allocator_type   x;return (x);}
 
         //  =============== PRIVATE ATTRIBUTES  =============== //
         private :
             // std::iterator<T>    _begin;
             // std::iterator<T>    _end;
-            unsigned long long      _nbElement;
-            unsigned long long      _maxElement;
-            T                       *_ptr;
-            std::allocator<T>       _allocator;
+            size_type               _nbElement;
+            size_type               _maxElement;
+            value_type              *_ptr;
+            allocator_type          _allocator;
+            
     };
      
 }
--- srcs/main.cpp
+++ srcs/main.cpp
@@ -6,17 +6,20 @@
 /*   By: aguay <aguay@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/24 13:20:00 by aguay             #+#    #+#             */
-/*   Updated: 2022/11/08 15:06:19 by aguay            ###   ########.fr       */
+/*   Updated: 2022/11/09 08:43:58 by aguay            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <string>
 #include "vector.hpp"
+#include <map>
+#include <vector>
 
 int	main(void)
 {
-    ft::vector<int> x;
+    int i = 42;
+    ft::vector<int> x(10, static_cast<int &>(i));
 
-    x.addObject(12);
 	return (0);
 }
